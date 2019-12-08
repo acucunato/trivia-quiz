16a049d3605fea06da40c1def0b5e512713d7439
Alyssa Cucunato <acucunato18@gmail.com>
2019-12-08 11:10:13 -0500

    finished styling, started js & pseudocode

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
+//define variables
+var startBtn = document.getElementById("start-btn");
+var startContainer = document.getElementById("start-container");
+var questionContainer = document.getElementById("question-container");
+var timeEl = document.getElementById("timeleft");
+
+var secondsLeft = 75;
+var userScore = 0;
+
+
+startBtn.addEventListener("click", startGame)
+
+function startGame() {
+    setTime();
+    console.log("started")
+    startContainer.classList.add("hide")
+    questionContainer.classList.remove("hide")
+
+
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        // sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+  function sendMessage() {
+      //send message when time is up function sendMessage() {
+//   timeEl.textContent = " ";
+
+//   var imgEl = document.createElement("img");
+
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+  }
+
+  function resetQuiz () {
+
+  }
+
+
 // // button to start quiz and set timer to 75 seconds
-// //first question 
 
 // //The first view of the application displays a button that starts the quiz.
 
--- questions.js
+++ questions.js
@@ -1,19 +1,19 @@
 //questions array with objects
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "In Harry Potter and the Scorcer's Stone, who is the Defense Against the Dark Arts teacher?",
+      choices: ["Professor Quirrell", "Gilderoy Lockhart", "Professor Snape", "Professor Slughorn"],
+      answer: "Professor Quirrell"
     },
     {
-      title: " ",
-      choices: [],
-      answer: " "
+      title: "In Harry Potter and the Chamber of Secrets, who fixes Harry's broken glasses in Diagon Alley?",
+      choices: ["Ron", "Hagrid", "Dumbledore", "Hermione"],
+      answer: "Hermione"
     },
     {
-        title: " ",
-        choices: [],
-        answer: " "
+        title: "What spell is Harry attempting under his sheets in the opening scene of the Prisioner of Azkaban?",
+        choices: ["Expecto Patronum", "Lumos Maximus", "Alohomora", "Protego" ],
+        answer: "Lumos Maximus"
       },
       {
         title: " ",
